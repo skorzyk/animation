08b7d76f8a45c5a7c6e774db0245e2f94d17e96e
Kamil Skorzynski <skorzynskikamil@gmail.com>
2020-06-18 12:29:16 +0200

    change for OOP and delete console.log

---
+++ Animbg.js
@@ -0,0 +1,20 @@
+class Animbg {
+  constructor(selector) {
+    this.elements = document.querySelectorAll(selector);
+  }
+  listenCoursorMove(event) {
+    const { clientX, clientY } = event;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    const posX = clientX - centerX;
+    const posY = clientY - centerY;
+    this.elements.forEach((element) => this.moveElement(element, posX, posY));
+  }
+  moveElement(element, posX, posY) {
+    const ratioX = -element.getAttribute('ratioX');
+    const ratioY = -element.getAttribute('ratioY');
+    element.style.transform = `translate(${posX * ratioX}px,${
+      posY * ratioY
+    }px)`;
+  }
+}
